0bd5c233759817360c61c1b11087dc9834abf094
EricEZz <zhangeric343@gmail.com>
2023-10-02 09:52:24 -0400

    a

---
+++ app.js
@@ -0,0 +1,22 @@
+/* //functions input output machine functions are declared and must be called 
+//functions can accept multiple arguments or parameters ,
+function test(mess){
+    //output
+ console.log(mess);
+}
+//run the function
+//numbewr data type
+test(101);
+
+//string data type ""
+test("balls")
+
+//booLeans true of false
+test(false) */
+
+function add (x,y){
+    console.log(x+y);
+}
+add(101,32);
+
+
