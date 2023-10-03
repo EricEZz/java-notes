6109c6e65750e62f69eb2b533d886c624d6bfd16
EricEZz <zhangeric343@gmail.com>
2023-10-03 10:11:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* //functions input output machine functions are declared and must be called 
+/* /* //functions input output machine functions are declared and must be called 
 //functions can accept multiple arguments or parameters ,
 function test(mess){
     //output
@@ -14,10 +14,25 @@ test("balls")
 //booLeans true of false
 test(false) */
 
-function add (x, y) {
-    console.log(x + y);
+// function add (x, y) {
+//     console.log(x + y);
+// }
+// //declare and define variable
+// const num1 = Number (prompt ("enter a number"));
+// const num2 = Number (prompt ("enter a number"));
+// //add(num1,num2);
+// //const does not allow definition to change
+// //let allows def to change 
+ // let x = 5;
+  // x = 6; 
+
+//global variable */
+const x = 7;
+//local variable function scope
+function test(x) {
+    let x = 45;
+    y = 34 ;
+    add(x,y);
 }
-//declare and define variable
-const num1 = Number (prompt ("enter a number"));
-const num2 = Number (prompt ("enter a number"));
-add(num1,num2);
+console.log(x);
+test();
