3e08e7f7b9830fc260f8075d20b91a091e3495b8
EricEZz <zhangeric343@gmail.com>
2023-10-13 09:51:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* /* //functions input output machine functions are declared and must be called 
+ /* //functions input output machine functions are declared and must be called 
 //functions can accept multiple arguments or parameters ,
 function test(mess){
     //output
@@ -27,6 +27,7 @@ test(false) */
   // x = 6; 
 
 //global variable */
+/*
 const x = 7;
 //local variable function scope
 function test(x) {
@@ -36,3 +37,58 @@ function test(x) {
 }
 console.log(x);
 test();
+function check(x) {
+    if (x <10);
+    console.log("small");
+ else if(x>10){
+    console.log("large nuber"); 
+} else {
+    console.log("error");
+}
+}
+function other (x,y){
+    if (x < 10 && y < 10){
+        console.log("smol number");
+    }else if (x>10 || y>10 ){
+        console.log("xome large numbers");
+    }
+    else{
+        console.log("all large numbers");
+    }
+}
+function getBMI(){
+    return
+}
+function checkBMI(){}
+
+function init(){
+    let BMI= getBMI(height,weight);
+    checkBMI(BMI);
+}
+
+const thing2 = {
+    firstName: "Colon",
+    lastName: "Semicolon",
+    graduated: false
+}
+*/
+
+
+{
+    const thing ={
+    firstName: "Chen-Zee",
+    lastName: "HeHe",
+    graduated: false,
+    DOB: 2007,
+    siblings: ["Chen Yee", "ChenWee"],
+    age: function(){
+        return year - this.DOB;
+    }
+}
+}
+//arrays store items and can access items by their index starting at 0
+const nums = [1, 2, 3, 4, 5];
+//console.log(nums[nums.length - 1 ]);
+//foreach to run function on every item (for loop)
+nums.forEach((el)=> console.log(el,index));
+//arrow functions ()=> are simpler, auto return its output, this keyword changes
