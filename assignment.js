1ac4db4d3ed62c872addf8f9b731df424308cee3
EricEZz <zhangeric343@gmail.com>
2023-11-10 09:40:23 -0500

    d

---
+++ assignment.js
@@ -0,0 +1,44 @@
+//const spots = []
+//const yesterday = ""
+//const today = ""
+/* for(let C = true;){
+    console.log(C);
+}
+
+//yesterday[0]
+//today [0]
+
+function filled(yay,nay){
+   let found
+    if(yay[C] === null){
+
+    }
+    else{
+
+    }
+
+} */
+//function spaces(s,y,t){
+  //  let total = 0;
+    //for(let i = 0; i<s; i++){
+      //  if(y[i]==="C"&& t[i]==="C"){
+        //    total++;
+//        }
+  //  }
+//    console.log(total);
+//}
+
+//fibonacci sequence 
+ 
+function fibonacci(num,a,b){
+    let total;
+    for (let i=2;i++; i<num){
+        a= b-1 + b-2 
+        const next = previous + current;
+        previous = current; 
+        current=next;
+        console.log current;
+    }
+return current;
+
+}
