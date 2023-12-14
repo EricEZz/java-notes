f1107364f311de2af3984e60753d5c88df346ac2
EricEZz <zhangeric343@gmail.com>
2023-12-14 10:06:43 -0500

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,40 @@
+/* let Angelina = {
+    name:"Angelina", //string 
+    age: 16, //number / integer 
+    Angela:false, // boolean 
+};
+ for(let i=0; i<Angelina.name.length; i++){
+    console.log(Angelina.name[i]);
+}
+let i = 0; 
+while(i<Angelina.name.length){
+    console.log(Angelina.name[i]);
+    i++; 
+}
+ 
+ function needle(name,search){
+    if(name === (search)){
+        return true;
+    }else {
+        return false;
+    }
+}
+console.log(needle(0, false)); 
+
+function sortString(name){
+    let x = [...name].sort; //[...name] spread operator Array.from(name)
+    console.log(x);
+}
+sortString(Angelina.name);
+
+let x = [1,2,3];
+let y = [4,5,6]; 
+let z = {...x,...y};
+console.log(z); 
+
+function removeL(name){
+    let x = [...name];
+    x.splice(-1);
+    console.log(x);
+}
+removeL(Angelina.name); */
